5ad0a36928223e0eda8508e06b53ca0e847353e5
jiriks74 <jiri.stefka.js@gmail.com>
2021-08-13 22:02:53 +0200

    Change dal - removed thrad name param, dal gets the name from calling thread

--- dal.py
+++ dal.py
@@ -1,3 +1,4 @@
+import threading
 import mysql.connector
 from decouple import config
 
@@ -32,6 +33,7 @@ class MySQLWrapper:
         Query of database. Returns list tuples from database.
         ;param query: str
         ;param val: Optional
+        ;param chache: bool (optional, True by defaut)
         ;return: list of tuples
         """
         self.cursor.execute(query, val or ()) # Execute query
@@ -48,7 +50,7 @@ class MySQLWrapper:
         Execute your values and commit them. Or not. Your decision.
         ;param query: str
         ;param val: Optional
-        ;param commit: bool
+        ;param commit: bool (optional, True by default)
         ;return: None
         """
         self.cursor.execute(query, val or ()) # Execute query
@@ -60,6 +62,11 @@ class data:
     """
     Class for working with numbers in database
     """
+
+    def __init__(self, **credentials) -> None:
+        if credentials:
+            self.credentials = credentials
+
     def tablename(self, number) -> str:
         """
         Get table name for a number
@@ -68,67 +75,85 @@ class data:
         """
         return f"{number // 1000000 + 1}m"
 
-    def write(self, number, next_number, thread) -> None:
+    def write(self, number, next_number) -> None:
         """
         Write working number and next_number into database
+        Sets 421_loop to False by default, and uses current thread name
         ;param number: int
         ;param next_number: int
-        ;param thread: int
         ;return: None
         """   
+        thread = threading.currentThread().getName()
+        tablename = self.tablename(number)
         with MySQLWrapper() as db:
-            db.execute(f"INSERT INTO `{self.tablename(number)}` (`number`, `next_number`, `421_loop`, `thread`) VALUES ('{number}', '{next_number}', '0', '{thread}');") 
+            db.execute(f"""CREATE TABLE IF NOT EXISTS `1m` (
+                `number` BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+                `next_number` BIGINT UNSIGNED NOT NULL,
+                `421_loop` BOOLEAN NOT NULL,
+                `thread` CHAR(107) NOT NULL
+                );""")
+            db.execute(f"INSERT INTO `{tablename}` (`number`, `next_number`, `421_loop`, `thread`) VALUES ('{number}', '{next_number}', '0', '{thread}');") 
 
-    def check(self, number) -> bool:
+    def check(self, number):
         """
-        Check if number exists in database and return if it falls into 421 loop
-        ;return: bool
+        Check if number exists in database and return if it falls into 421 loop.
+        If another number is working with passed number, this function will wait, until the other thread is done.
+        If this number is working with this number, this function will return 421_loop as False, as the number exists, but the thread shouldn't come to one number twice if this math problem is true.
+        ;param: number
+        ;return: bool (number exists)
+        ;return: bool (421_loop)
         """
+        thread = threading.currentThread().getName()
+
         with MySQLWrapper() as db:
             tabname = self.tablename(number)
             while True: # Loop so I can check the values repeatedly when other thread is working on them
 
                 table = db.query(f"SELECT `421_loop`, `thread` FROM `{tabname}` WHERE `number`='{number}';", cache=False)
 
-                if table[0][1] == 0: # Check if other thread is working with this number (all data should be on first row) (other than None means thread is working on it)
-                    
-                    if table[0][0] == 1: # If true return true
-                        return True
+                print(table)
+                if len(table) == 0:
+                    return False, False
 
-                    elif table[0][0] == 0: # If false return false
-                        return False
+                else:
+                    if table[0][1] == 0: # Check if other thread is working with this number (all data should be on first row) (other than None means thread is working on it)
+                        
+                        if table[0][0] == 1: # If 421 is true return true
+                            return True, True
 
-                    else: # Else - nonsense value has appeared in database - WTF? JustInCase
-                        print(f"""Nonsense in database was found:
-                        In table: {tabname}
-                        At number: {number}""")
+                        elif table[0][0] == 0: # If 421 is false return false
+                            return True, False
 
-                        exit()
+                        else: # Else - nonsense value has appeared in database - WTF? JustInCase
+                            print(f"""Nonsense in database was found:
+                            In table: {tabname}
+                            At number: {number}""")
 
-    def set_loop(self, start_number, thread) -> None:
+                            exit()
+                    elif table[0][1] == thread: # If the current thread created this number, loop other than 421 has been found: number exists, but it isn't 421
+                        return True, False
+
+    def set_loop(self, start_number) -> None:
         """
         Set nuber sequence in database to 421_loop: true
         ;param start_number: int
-        ;param thread: int
         ;return: None
         """
+        thread = threading.currentThread().getName()
+
         with MySQLWrapper() as db:
             acnum = start_number # Active number
             while True: # Used to iterate through numbers in database
                 tabname = self.tablename(acnum) # Get the table of the number
                 table = db.query(f"SELECT `number`, `next_number`, `421_loop`, `thread` FROM `{tabname}` WHERE `number`='{acnum}';") # Get all the data about the number
 
-                if table[0][2] == 0: # Check if 421_loop is set to false (othervise stop)
-                    if table[0][3] == thread: # If the thread number is right
+                if table[0][3] == thread: # Check if number belongs to current thread
+                    if table[0][2] == 0: # Check if loop is already set
                         db.execute(f"UPDATE `{tabname}` SET `421_loop`='1', `thread`='0' WHERE `number`='{acnum}';", commit=False) # Set thread to 0 (as no thread is working on this number) and update 421_loop to true
                         acnum = table[0][1] # Get next number
 
                     else: 
-                        print(f"""Nonsense occured - looks like a thread is working with number that it shouldn't be:
-                        Table: {tabname}
-                        Number: {acnum}
-                        No data was committed""")
-                        exit()
+                        break
 
                 else:
                     break
@@ -138,4 +163,5 @@ class data:
 
 
 if __name__ == "__main__":
-    print(data().check(1))
+    if False: print("yup")
+    print(data().check(5))
--- main.py
+++ main.py
@@ -29,25 +29,37 @@ class compute:
             while self.q.queue[0] == 1:
                 pass
 
-            self.q.queue[1] = f"{thread_name}: {datetime.now()}"
+            self.q.queue[1] = f"{threading.currentThread().getName()}: {datetime.now()}"
             self.q.queue[0] = 1
             #print(f"Message put into q and flag was set.\n The message was: \'{q.get()}\'")
             time.sleep(delay)
 
-    def compute(self) -> None:
-        acnum = 1
+    def compute(self, startnum) -> None:
+        acnum = startnum
         dt = data()
         while True:
-            if dt.check(acnum):
+            exists, loop = dt.check(acnum)
+            if not exists:
                 if acnum % 2 == 0:
-                    pass
+                    next_num = acnum / 2
+                    dt.write(acnum, next_num, False, threading.currentThread().getName())
+                    acnum = next_num
+
                 else:
-                    pass
+                    next_num = 3 * acnum + 1
+                    dt.write(acnum, next_num, False, threading.currentThread().getName())
+                    acnum = next_num
             else:
-                print(f"""Probably error:
-                Number {acnum} doesn't fall into 421_loop
-                """)
-                exit()
+                if loop:
+                    dt.set_loop(startnum, threading.currentThread().getName())
+                    break
+                if not loop:
+                    dt.set_loop(startnum, threading.currentThread().getName())
+                    print(f"""Non 421 number found!:
+                    Startnum: {startnum}
+                    Table: {startnum // 1000000 +1}m""")
+                    exit()
+
 
 if __name__ == "__main__":
     compute().start_threads()
